ae1e32bff3122e9c299d38e46bc2c54b716f7011
Zach Gates <thezachgates@gmail.com>
2018-06-14 09:48:42 -0500

    Update FileManager

--- common.py
+++ common.py
@@ -1,32 +1,58 @@
 import os
 
 from . import constants
-from .base import FileError
+from .base import File, FileError
 from .xmllib.base import XMLFile
+
+from panda3d.core import ConfigVariableString
 
 
 class FileManager(object):
 
     ROOT = ''
 
-    def __init__(self, root=''):
+    def __init__(self, root=None):
         object.__init__(self)
-        self.__setRoot(root or vfs.getCwd())
+        self.ROOT = str(root or FileManager.ROOT or vfs.getCwd())
 
-    def __setRoot(self, abspath):
-        self.ROOT = str(abspath)
+    @property
+    def root(self):
+        return self.ROOT
 
-    def getFile(self, relpath):
-        return vfs.findFile(relpath, ROOT)
+    @property
+    def model_ext(self):
+        return ConfigVariableString('default-model-extension').getValue()
 
-    def getFiles(self, relpaths):
-        for relpath in relpaths:
-            virtualFile = self.getFile(relpath)
-            yield File(virtualFile.getFilename())
+    def find(self, relpath):
+        return vfs.findFile(relpath, self.root)
+
+    def scan(self, relpath, filesOnly=False, foldersOnly=False):
+        for virtualFile in vfs.scanDirectory(relpath):
+            if filesOnly and not virtualFile.isRegularFile():
+                continue
+            elif foldersOnly and not virtualFile.isDirectory():
+                continue
+            else:
+                yield virtualFile
+
+    def loadDirectory(self, virtualLink, modelsOnly=None):
+        if virtualLink and virtualLink.isDirectory():
+            for virtualFile in virtualLink.scanDirectory().getFiles():
+                fname = virtualFile.getFilename()
+                if modelsOnly and fname.getExtension() != self.model_ext[1:]:
+                    continue
+                else:
+                    yield fname
+
+    def loadFile(self, virtualFile):
+        if virtualFile:
+            return File(virtualFile.getFilename())
+        else:
+            return None
 
     def loadXML(self, virtualFile):
         try:
             with XMLFile(virtualFile.getFilename()) as fobj:
                 return fobj
         except FileError as e:
-            pass
+            return None
