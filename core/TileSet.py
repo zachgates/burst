d926d9fafa61283f2958dedf6285da5de91d7aa8
Zach Gates <thezachgates@gmail.com>
2020-08-24 22:25:25 -0500

    Trust-busting

--- core/TileSet.py
+++ core/TileSet.py
@@ -7,97 +7,13 @@ from panda3d import core as p3d
 
 from direct.directnotify import DirectNotifyGlobal
 
-from .TexturePool import TexturePool
+from .PixelSet import PixelSet
+from .XYDataset import XYDataset
 from .XYRuleset import XYRuleset
+from ..tools.TexturePool import TexturePool
 
 
 LOG = DirectNotifyGlobal.directNotify.newCategory(__name__)
-
-
-class XYDataset(object):
-
-    _DTYPE = p3d.LPoint2i
-    _EMPTY = p3d.LVector4i.zero()
-
-    def __init__(self, width, height, data, mode):
-        self.__size = p3d.LVector2i(width, height)
-        self.__mode = mode
-        self._data = memoryview(data)
-        self._grid = {}
-
-    @property
-    def width(self):
-        return self.__size.x
-
-    @property
-    def height(self):
-        return self.__size.y
-
-    def get(self, point: _DTYPE):
-        return tuple(self._grid.get(point, self._EMPTY))
-
-    def select(self, start: [int, _DTYPE], end: [int, _DTYPE] = None):
-        """
-        Generate a slice of the 2D (X, Y) matrix.
-        """
-        try:
-            # Getting a single "start" point
-            if end is None:
-                cell = None
-                try:
-                    # Get the point by index
-                    cell = self[start]
-                except TypeError:
-                    # Get the point from the grid
-                    assert isinstance(start, self._DTYPE)
-                    cell = self.get(start)
-                finally:
-                    # Wrap the single cell
-                    yield iter([cell])
-            else:
-                try:
-                    # Get points by index
-                    yield (self[n] for n in range(start, end + 1))
-
-                except TypeError:
-                    # Get points from the grid
-                    assert isinstance(start, self._DTYPE)
-                    assert isinstance(end, self._DTYPE)
-
-                    # Generate [start, end] slice
-                    for x in range(start.x, end.x + 1):
-                        yield (self.get(self._DTYPE(x, y))
-                               for y in range(start.y, end.y + 1))
-
-        except AssertionError:
-            LOG.warning(f'invalid selection: {start} -> {end}')
-            return None
-
-
-class PixelSet(XYDataset):
-
-    _BLANK = p3d.LVector4i.zero()
-
-    def __init__(self, tex: p3d.Texture, mode = 'BGRA'):
-        if tex and tex.hasRamImage():
-            width, height = (tex.getXSize(), tex.getYSize())
-            data = tex.getRamImageAs(mode)
-        else:
-            width = height = 0
-            data = b''
-
-        super().__init__(width, height, data, mode)
-
-        for offset in range(0, len(data), len(mode)):
-            # Determine cell data
-            color = self._data[offset : offset + len(mode)]
-            index = offset // len(mode)
-            point = p3d.LPoint2i(
-                x = self.height - (index // self.width),
-                y = index % self.width + 1)
-
-            # Define the cell
-            self._grid[point] = color
 
 
 class TileSet(TexturePool):
