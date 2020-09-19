4e868707631ca46f1a25154bd98db43a4ba7dd52
Zach Gates <thezachgates@gmail.com>
2020-09-18 23:16:07 -0500

    Better organize module __init__

--- __init__.py
+++ __init__.py
@@ -1,5 +1,22 @@
-from panda3d.core import loadPrcFileData
-loadPrcFileData('burst',
+import builtins
+import importlib
+
+from panda3d import core as p3d
+
+
+class _burst(type):
+    def __new__(cls, name, bases, dct):
+        for func in importlib.import_module('burst.core.tools').__all__:
+            dct[func.__name__] = staticmethod(func)
+        return super().__new__(cls, name, bases, dct)
+
+
+class burst(metaclass = _burst):
+    pass
+
+
+builtins.burst = burst()
+p3d.loadPrcFileData('burst',
     """
     load-display pandagl
     win-origin -2 -2
@@ -9,14 +26,3 @@ loadPrcFileData('burst',
     textures-square none
     textures-power-2 none
     """)
-
-
-import importlib
-tools = importlib.import_module('burst.core.tools')
-class burst:
-    for func in tools.__all__:
-        locals()[func.__name__] = staticmethod(func)
-
-
-import builtins
-builtins.burst = burst()
