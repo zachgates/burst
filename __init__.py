fa35defe4df158ea8d862d423b1485772e5b28fe
Zach Gates <thezachgates@gmail.com>
2020-09-19 01:57:42 -0500

    Further improve module __init__

--- __init__.py
+++ __init__.py
@@ -1,28 +1,54 @@
-import builtins
-import importlib
-
-from panda3d import core as p3d
+from . import core, scene, tile
 
 
-class _burst(type):
+class Burst(type):
+
     def __new__(cls, name, bases, dct):
-        for func in importlib.import_module('burst.core.tools').__all__:
-            dct[func.__name__] = staticmethod(func)
-        return super().__new__(cls, name, bases, dct)
+        # Use builtins to store the burst singleton.
+        import builtins
+
+        # Create the global burst object.
+        if not hasattr(builtins, 'burst'):
+            builtins.burst = super().__new__(cls, name, bases, dct)
+            builtins.burst.attach_tools()
+
+        # Only ever return the singleton object.
+        return builtins.burst
+
+    def attach_tools(cls):
+        # The tools module is not exported from core; import it for use here.
+        import burst.core.tools
+
+        # Hook the core tool functions to the global burst object.
+        for func in burst.core.tools.__all__:
+            setattr(cls, func.__name__, staticmethod(func))
+
+        # Now remove tools from core; limiting use to explicit imports only.
+        del burst.core.tools
 
 
-class burst(metaclass = _burst):
-    pass
+class Burst(metaclass = Burst):
+
+    # Hook panda3d.core to burst.p3d
+    from panda3d import core as p3d
+
+    p3d.loadPrcFileData(
+        """
+        Burst module initial PRC data:
+            - win-origin: puts the window at the center of the screen.
+            - win-size: smallest possible; loading a scene adjusts the size.
+            - textures-square: set none to support non-square tilesheets.
+            - textures-power-2: set none to support non-power-of-2 tilesheets.
+        """,
+        """
+        load-display pandagl
+        win-origin -2 -2
+        win-size 1 1
+        fullscreen #f
+        notify-level info
+        textures-square none
+        textures-power-2 none
+        """)
 
 
-builtins.burst = burst()
-p3d.loadPrcFileData('burst',
-    """
-    load-display pandagl
-    win-origin -2 -2
-    win-size 1 1
-    fullscreen #f
-    notify-level info
-    textures-square none
-    textures-power-2 none
-    """)
+__all__ = [core, scene, tile, Burst]
