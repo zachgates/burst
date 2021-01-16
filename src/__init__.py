311fa7c61f71e0946603f42fa7e9f05a088b99d1
Zach Gates <thezachgates@gmail.com>
2021-01-16 02:12:06 -0600

    Remove unneeded __init__

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-
-__all__ = ['base', 'control', 'nodes', 'xmllib']
-
-
-for submodule in __all__:
-    importlib.import_module('.' + submodule, __name__)
--- src/xmllib/__init__.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-
-__all__ = ['base']
-
-
-for submodule in __all__:
-    importlib.import_module('.' + submodule, __name__)
