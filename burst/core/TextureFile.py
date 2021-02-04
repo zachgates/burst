6cba296d896bf99bdce9e4470b1ebf81f89f97f2
Zach Gates <thezachgates@gmail.com>
2021-02-04 09:40:11 -0600

    Change default alpha kwarg

--- burst/core/TextureFile.py
+++ burst/core/TextureFile.py
@@ -12,7 +12,7 @@ class TextureFile(burst.control.File, extensions = ['.jpg', '.png', '.gif']):
 
     def read(self, /, *,
              alpha: Union[bool, str, pathlib.Path, p3d.Filename,
-                          burst.control.File] = None,
+                          burst.control.File] = False,
              ) -> p3d.Texture:
         """
         Attempts to load the TexFile as a Texture. An alpha File/path may be
@@ -20,9 +20,9 @@ class TextureFile(burst.control.File, extensions = ['.jpg', '.png', '.gif']):
         """
         if alpha is True:
             if not (alpha := self.path.with_suffix('.rgb')).exists():
-                alpha = None
+                alpha = False
 
-        if alpha is None:
+        if alpha is False:
             return base.loader.load_texture(self.path)
 
         if isinstance(alpha, p3d.Filename):
