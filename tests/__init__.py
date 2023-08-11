1cfda818e23e92a8c3e3fa233b3f56074af8d46b
Zach Gates <thezachgates@gmail.com>
2023-08-11 16:56:50 -0500

    Fix

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,5 +13,15 @@ __all__ = [
 import burst
 import pathlib
 
+from panda3d import core as p3d
+
 
 burst.store.add_search_path(pathlib.Path(__file__).with_name('data'))
+
+p3d.load_prc_file_data(
+    "",
+    """
+    textures-auto-power-2 #f
+    textures-power-2 #f
+    """,
+    )
