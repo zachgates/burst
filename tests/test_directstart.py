bb8ee34acac81d627f48b695382b1f204c794787
Zach Gates <thezachgates@gmail.com>
2023-08-11 17:04:20 -0500

    Fix tests

--- tests/test_directstart.py
+++ tests/test_directstart.py
@@ -1,28 +1,35 @@
 import burst
+import typing
 
 import panda3d.core as p3d
 
 from direct.directbase.DirectStart import base
 
 
-def StaticFrame(scene, index: int) -> p3d.NodePath:
+def StaticFrame(scene, cell: tuple[int, int]) -> p3d.PandaNode:
     """Simple factory for static frames."""
-    return scene.make_tile(index)
+    return scene.make_tile(cell).node()
 
 
-def SpriteFrame(scene, name: str, frames = []) -> p3d.NodePath:
+def SpriteFrame(scene,
+                name: str,
+                cells: typing.Iterable[tuple[int, int]] = [],
+                ) -> p3d.SequenceNode:
     """Simple factory for animated frames."""
     node = p3d.SequenceNode(name)
-    for index in frames:
-        np = StaticFrame(scene, index)
-        node.add_child(np.node())
+    for cell in cells:
+        node.add_child(StaticFrame(scene, cell))
     return node
 
 
 if __name__ == '__main__':
     scene = burst.store.load_file('tests/data/scenes/sample.burst2d').read()
-    sprite = SpriteFrame(scene, 'Actor-Foo', [275, 279, 277, 278])
+    aspect2d.attach_new_node(sprite := SpriteFrame(scene, 'sprite', [
+        (9, 19),
+        (9, 23),
+        (9, 21),
+        (9, 22),
+        ]))
     sprite.set_frame_rate(12)
     sprite.pingpong(True)
-    aspect2d.attach_new_node(sprite)
     base.run()
--- tests/test_showbase.py
+++ tests/test_showbase.py
@@ -1,5 +1,9 @@
+import burst
+
 import panda3d.core as p3d
 
+from direct.interval.IntervalGlobal import Func, Parallel, Sequence
+from direct.interval.LerpInterval import LerpPosInterval
 from direct.showbase.ShowBase import ShowBase
 
 
@@ -9,14 +13,46 @@ class SampleScene(ShowBase):
         super().__init__()
         file = burst.store.load_file('tests/data/scenes/sample.burst2d')
         self.scene = file.read()
+        self.scene.adjust_window('sample', (512, 128))
+
+    def move_sprite(self, *extraArgs):
+        if not hasattr(self, 'lerp'):
+            self.lerp = Sequence(
+                Parallel(
+                    Func(self.sprite.play),
+                    self.np.posInterval(
+                        (24 / 60),
+                        pos = p3d.Point3(self.np.get_x() + extraArgs[0], 0, 0),
+                        startPos = self.np.get_pos(),
+                        name = 'sprite-lerp',
+                        )),
+                Func(lambda: delattr(self, 'lerp')),
+                ).start()
 
     def run(self):
-        sprite = p3d.SequenceNode('sprite')
-        for n in (307, 308, 309, 310):
-            sprite.add_child(self.scene.make_tile(n).node())
-        sprite.set_frame_rate(12)
-        sprite.pingpong(True)
-        aspect2d.attach_new_node(sprite)
+        globalClock.set_mode(p3d.ClockObject.MLimited)
+        globalClock.set_frame_rate(60)
+
+        self.sprite = p3d.SequenceNode('sprite')
+        for cell in ((10, 19),
+                     (10, 20),
+                     (10, 21),
+                     (10, 22),
+                     (10, 22),
+                     (10, 21),
+                     (10, 20),
+                     (10, 19),
+                     ):
+            self.sprite.add_child(self.scene.make_tile(cell).node())
+        self.sprite.set_frame_rate(24)
+        self.np = base.aspect2d.attach_new_node(self.sprite)
+
+        self.accept('space', self.sprite.play)
+        self.accept('arrow_right', self.move_sprite, extraArgs = [1])
+        self.accept('arrow_right-repeat', self.move_sprite, extraArgs = [1])
+        self.accept('arrow_left', self.move_sprite, extraArgs = [-1])
+        self.accept('arrow_left-repeat', self.move_sprite, extraArgs = [-1])
+
         super().run()
 
 
