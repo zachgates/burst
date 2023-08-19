1109c493061ff4467c1a6a4165266df14163011b
Zach Gates <thezachgates@gmail.com>
2023-08-19 12:11:40 -0500

    Fix

--- tests/test_directstart.py
+++ tests/test_directstart.py
@@ -6,7 +6,7 @@ from direct.directbase.DirectStart import base
 if __name__ == '__main__':
     scene = burst.store.load_file('data/scenes/sample1.burst2d').read()
     scene.set_frame_rate(60)
-    sprite = burst.char.Sprite(scene, 'sprite')
+    sprite = burst.character.Sprite(scene, 'sprite')
     sprite.add_track('Idle', [(9, 19), (9, 23), (9, 22), (9, 21)], frame_rate = 8)
     sprite.loop('Idle')
     spriteNP = base.aspect2d.attach_new_node(sprite)
