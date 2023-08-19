2ca6ac921bb16368a7aff9ddca1dc9e8f02eefd3
Zach Gates <thezachgates@gmail.com>
2023-08-18 22:41:46 -0500

    Fix

--- tests/test_directstart.py
+++ tests/test_directstart.py
@@ -5,6 +5,7 @@ from direct.directbase.DirectStart import base
 
 if __name__ == '__main__':
     scene = burst.store.load_file('tests/data/scenes/sample1.burst2d').read()
+    scene.set_frame_rate(60)
     sprite = burst.char.Sprite(scene, 'sprite')
     sprite.add_track('Idle', [(9, 19), (9, 23), (9, 22), (9, 21)], frame_rate = 8)
     sprite.loop('Idle')
